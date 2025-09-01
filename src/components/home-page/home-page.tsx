184fff2a1cf24f2c4755c7904a56dd9f37895047
Justine Coulon <justinecoulon1@gmail.com>
2025-09-01 09:04:17 +0200

    fixed no fetching on app start

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,12 +1,16 @@
 import styles from './home-page.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMetamobMonstersQuery } from '../../utils/api/metamob.queries';
 import MonstersContainer from './monsters-container/monsters-container';
 import FiltersContainer from './filters-container/filters-container';
 
 export default function HomePageContainer() {
-    const { data: allMonsters, isLoading, error } = useMetamobMonstersQuery();
+    const { data: allMonsters, isLoading, error, refetch } = useMetamobMonstersQuery();
     const [filteredMonsters, setFilteredMonsters] = useState(allMonsters);
+
+    useEffect(() => {
+        void refetch();
+    }, []);
 
     return (
         <div className={styles.homePageContainer}>
